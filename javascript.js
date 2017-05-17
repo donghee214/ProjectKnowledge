b9e16a9cbef4a1c61de21989974323c38ab44ae7
donghee214 <donghee214@gmail.com>
2017-05-17 19:42:21 -0400

    most updated so far

--- javascript.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    Typed.new(".element", {
-        strings: ["First sentence.", "Second sentence."],
-        typeSpeed: 0
-    });
-});
