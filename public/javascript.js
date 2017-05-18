a47cbd1a7488336ddddbce21c39e0735dc4400a2
donghee214 <donghee214@gmail.com>
2017-05-18 10:52:17 -0400

    First commit

--- public/javascript.js
+++ public/javascript.js
@@ -1,23 +1,41 @@
 var ranOnce = false;
 var ranOnce1 = false;
 var ranOnce2 = false;
+var ranOnce3 = false;
+var ranOnce4 = false;
 
 function testScroll(ev) {
+    if ((window.pageYOffset > 740) && ranOnce3 == false) {
+        var event1Center = document.getElementById("bio");
+        event1Center.className += " nowVisible";
+        ranOnce3 = true;
+    };
+    if ((window.pageYOffset > 1450) && ranOnce4 == false) {
+        var event2Center = document.getElementById("bio1");
+        var event2Center1 = document.getElementById("bio2");
+        var event2Center2 = document.getElementById("bio3");
+        event2Center.className += " nowVisible";
+        event2Center1.className += " nowVisible";
+        event2Center2.className += " nowVisible";
+        ranOnce4 = true;
+    };
     if ((window.pageYOffset > 1600) && ranOnce == false) {
         var event1Left = document.getElementById("uno");
         event1Left.className += " showDisplay";
         var event1Right = document.getElementById("dos");
         event1Right.className += " showDisplay";
         ranOnce = true;
+        var event1Center = document.getElementById("bio");
+        event1Center.className += " nowVisible";
     };
-    if ((window.pageYOffset > 1800) && ranOnce1 == false) {
+    if ((window.pageYOffset > 2000) && ranOnce1 == false) {
         var event2Left = document.getElementById("trois");
         event2Left.className += " showDisplay";
         var event2Right = document.getElementById("quatro");
         event2Right.className += " showDisplay";
         ranOnce1 = true;
     };
-    if ((window.pageYOffset > 2000) && ranOnce2 == false) {
+    if ((window.pageYOffset > 2400) && ranOnce2 == false) {
         var event3Left = document.getElementById("cinco");
         event3Left.className += " showDisplay";
         var event3Right = document.getElementById("six");
@@ -69,3 +87,8 @@ function scroll3() {
         }
     }, y);
 };
+
+
+var modal = document.getElementById("omgWork");
+modal.className += " bringModal";
+
